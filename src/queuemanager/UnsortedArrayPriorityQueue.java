d93005be6eaae8b19b1c2c5ffb6d040f4638483a
RichardColdwell <15017492@uhi.ac.uk>
2018-03-07 11:11:28 +0000

    Added the UnsortedArrayPriorityQueue Class updated QueueManager so UA option work from menu unsorted array working apart from remove

---
+++ src/queuemanager/UnsortedArrayPriorityQueue.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuemanager;
+
+/**
+ *
+ * @author Richar coldwell
+ */
+public class UnsortedArrayPriorityQueue<T>implements PriorityQueue<T> {
+    
+    /**
+     * Where the data is actually stored.
+     */
+    private final Object[] storage;
+
+    /**
+     * The size of the storage array.
+     */
+    private final int capacity;
+
+    /**
+     * The index of the last item stored.
+     *
+     * This is equal to the item count minus one.
+     */
+    private int tailIndex;
+
+    /**
+     * Create a new empty queue of the given size.
+     *
+     * @param size
+     */
+    public UnsortedArrayPriorityQueue(int size) {
+        storage = new Object[size];
+        capacity = size;
+        tailIndex = -1;
+    }
+    
+    
+    @Override
+    public T head() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            return ((PriorityItem<T>) storage[0]).getItem();
+        }
+    }
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        tailIndex = tailIndex + 1;
+        if (tailIndex >= capacity) {
+            /* No resizing implemented, but that would be a good enhancement. */
+            tailIndex = tailIndex - 1;
+            throw new QueueOverflowException();
+        } else {
+            storage[tailIndex] = new PriorityItem<>(item, priority);
+        }
+    }
+    
+    
+    @Override
+    public void remove() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            /*for (int i = 0; i < tailIndex; i++) {
+                storage[i] = storage[i + 1];
+            }
+            tailIndex = tailIndex - 1;*/
+        }
+    }
+    
+    
+    
+    @Override
+    public boolean isEmpty() {
+        return tailIndex < 0;
+    }
+
+    @Override
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i <= tailIndex; i++) {
+            if (i > 0) {
+                result = result + ", ";
+            }
+            result = result + storage[i];
+        }
+        result = result + "]";
+        return result;
+    }
+}
